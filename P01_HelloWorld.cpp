3c3507c3d353859b024dbcedfd3a3a6a71671aa3
VirajParmar96 <viraj.parmar960@gmail.com>
2025-05-05 11:00:52 +0530

    1. Hello World Program

---
+++ P01_HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+  Program: Hello World
+  Level  : Basic
+  Author : Viraj Parmar
+  Description:
+    A simple C++ program to print "Hello, World!" on the screen.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+// Output:
+// Hello, World!
