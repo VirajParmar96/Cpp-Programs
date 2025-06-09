1c354882ef0f314f74bc50616893e94d0fc40d96
VirajParmar96 <viraj.parmar960@gmail.com>
2025-06-09 10:18:51 +0530

    54. Count Vowels in a String

---
+++ P54_CountVowels.cpp
@@ -0,0 +1,37 @@
+/*
+  Program: Count Vowels in a String
+  Level  : Basic
+  Author : Viraj Parmar
+  Description:
+    A program to count the number of vowels in a given string.
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isVowel(char ch) {
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+
+int main() {
+    char str[100];
+    int vowelCount = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (isVowel(str[i]))
+            vowelCount++;
+    }
+
+    cout << "Total number of vowels: " << vowelCount << endl;
+
+    return 0;
+}
+
+
+// Output:
+// Enter a string: Hello OpenAI
+// Total number of vowels: 6
